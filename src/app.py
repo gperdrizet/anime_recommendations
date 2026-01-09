6bfe13c2b4f1c9bbadfa10dcbad6371cb1e857d1
George Perdrizet <george@perdrizet.org>
2026-01-09 03:36:28 +0000

    Switched to parquet format for storage of processed anime dataframe

--- src/app.py
+++ src/app.py
@@ -1,3 +1,50 @@
 '''Simple recommendation system for anime titles using content based filtering & Streamlit'''
 
+import pandas as pd
+import streamlit as st
 
+# Load the data
+ANIMES_DF = pd.read_csv('../data/processed_animes.parquet')
+
+def genre_similarity(genres1, genres2):
+    """Calculate Jaccard similarity between two genre sets"""
+
+    # Return 0 if either set is empty
+    if len(genres1) == 0 or len(genres2) == 0:
+        return 0
+
+    print(type(genres1))
+
+    # Calculate intersection (common genres) and union (all unique genres)
+    intersection = len(genres1.intersection(genres2))
+    union = len(genres1.union(genres2))
+
+    # Jaccard similarity = intersection / union
+    return intersection / union if union > 0 else 0
+
+
+def get_anime_recommendations(target_anime_id: int):
+    '''Takes an anime as input, returns top 5 most similar
+    animes based on genera.'''
+
+    target_anime = ANIMES_DF[ANIMES_DF['anime_id'] == target_anime_id].iloc[0]
+    target_genres = target_anime['genre_set']
+
+    print(f"Target anime: {target_anime['name']}")
+    print(f"Genres: {target_anime['genre']}")
+
+    ANIMES_DF['similarity'] = ANIMES_DF['genre_set'].apply(
+        lambda x: genre_similarity(target_genres, x)
+    )
+
+    similar_animes = ANIMES_DF[ANIMES_DF['anime_id'] != target_anime_id].sort_values(
+        'similarity', 
+        ascending=False
+    )[['name', 'genre', 'similarity']].head(5)
+
+    return similar_animes
+
+
+similar_animes = get_anime_recommendations(1)
+
+print(similar_animes)
